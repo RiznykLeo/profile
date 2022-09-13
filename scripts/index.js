d3031b0c04314a8390e98d23fc74d07045a189e2
leonid riznyk <riznyk.leo@gmail.com>
2022-09-13 16:52:33 +0900

    feat responsive nav

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,34 @@
-const mySkills = [
-    'HTML', 'CSS', 'SCSS',
-    'Bootstrap', 'Git',
-    'Figma', 'PostgresSQL', 'Heroku',
-    'Javascript', 'Github', 'Ruby on Rails',
-];
-TagCloud(".skills", mySkills, {
-    radius: 160,
-    maxSpeed: 'normal',
-    initSpeed: 'slow',
-    direction: 135,
-    keep: true
-});
-
 const nav = document.querySelector(".nav")
 
-window.onscroll = (event) => {
-
-    // print "false" if direction is down and "true" if up
-    const scroll = this.oldScroll > this.scrollY;
-
-    scroll ? nav.classList.remove("scroll-up") : nav.classList.add("scroll-up");
-
-    this.oldScroll = this.scrollY;
+function skills() {
+    const mySkills = [
+        'HTML', 'CSS', 'SCSS',
+        'Bootstrap', 'Git',
+        'Figma', 'PostgresSQL', 'Heroku',
+        'Javascript', 'Github', 'Ruby on Rails',
+    ];
+    TagCloud(".skills", mySkills, {
+        radius: 160,
+        maxSpeed: 'normal',
+        initSpeed: 'slow',
+        direction: 135,
+        keep: true
+    });
 }
 
+function navScroll() {
+    window.onscroll = (event) => {
+        const scroll = this.oldScroll > this.scrollY;
+        scroll ? nav.classList.remove("scroll-up") : nav.classList.add("scroll-up");
+        this.oldScroll = this.scrollY;
+    }
+}
+
+function burger() {
+    if (nav.classList.contains("responsive")) {
+        nav.classList.remove("responsive")
+    } else {
+        nav.classList.add("responsive")
+    }
+}
 
